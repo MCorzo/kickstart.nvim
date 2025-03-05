0fbb9d945d5d91cedb6162e1fc3312cb7e9a38c3
Miguel Corzo Lequerica <miguel.corzo@outlook.com>
2025-03-05 01:34:01 -0500

    fix: actualizada la configuracion de which-key.nvim
    se actualiza la configuración del mapeo de teclas para el plugin
    which-key.nvim

--- init.lua
+++ init.lua
@@ -173,19 +173,33 @@ require('lazy').setup({
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+    keys = {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>t', group = '[T]elescope' },
+      { '<leader>t_', hidden = true },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
+    },
+    --config = function() -- This is the function that runs, AFTER loading
+    --require('which-key').setup()
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]elescope', _ = 'which_key_ignore' },
-      }
-    end,
+    -- Document existing key chains
+    --require('which-key').register {
+    --  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+    --  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+    --  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+    --  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+    --  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    --  ['<leader>t'] = { name = '[T]elescope', _ = 'which_key_ignore' },
+    --}
+    --end,
   },
 
   -- NOTE: Plugins can specify dependencies.
@@ -466,7 +480,7 @@ require('lazy').setup({
 
       local servers = {
         angularls = {},
-        tsserver = {},
+        --tsserver = {},
         html = {},
         omnisharp = {
           capabilities = capabilities,
